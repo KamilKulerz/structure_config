120f183a67f521ccbbf603f15d327d710982a5c5
LAPTOP-5C4O9PKD\Kamil <kamil.kulerz@gmail.com>
2021-12-05 22:02:13 +0100

    separating data

---
+++ data.js
@@ -0,0 +1,102 @@
+var listOfIcons = [
+    'icon1.png',
+    'icon2.png',
+    'icon3.png'
+]
+
+var flatListOfCards = [{
+        "name": "card55",
+        "url": "",
+        "image": "icon1.png",
+        "title": "card55",
+        "zoom": "1",
+        "type": "tile",
+        "section": "top"
+    },
+    {
+        "name": "card11",
+        "url": "",
+        "image": "icon3.png",
+        "title": "card11",
+        "zoom": "1",
+        "type": "tile",
+        "section": "top"
+    },
+    {
+        "name": "card57",
+        "url": "https://google.com",
+        "image": "icon2.png",
+        "title": "card57",
+        "zoom": "1",
+        "type": "web",
+        "section": "top"
+    },
+
+    {
+        "name": "card99",
+        "url": "https://google.com",
+        "image": "icon1.png",
+        "title": "card99",
+        "zoom": "1",
+        "type": "web",
+        "section": "card55"
+    },
+
+    {
+        "name": "card102",
+        "url": "",
+        "image": "icon3.png",
+        "title": "card102",
+        "zoom": "1",
+        "type": "file",
+        "section": "card11"
+    },
+    {
+        "name": "card3",
+        "url": "",
+        "image": "icon2.png",
+        "title": "card3",
+        "zoom": "1",
+        "type": "tile",
+        "section": "card11"
+    },
+
+
+    {
+        "name": "card13",
+        "url": "https://google.com",
+        "image": "icon1.png",
+        "title": "card13",
+        "zoom": "1",
+        "type": "web",
+        "section": "card57"
+    },
+    {
+        "name": "card501",
+        "url": "",
+        "image": "icon3.png",
+        "title": "card501",
+        "zoom": "1",
+        "type": "file",
+        "section": "card3"
+    },
+    {
+        "name": "card2",
+        "url": "",
+        "image": "icon2.png",
+        "title": "card2",
+        "zoom": "1",
+        "type": "tile",
+        "section": "card3"
+    },
+
+    {
+        "name": "card1",
+        "url": "https://google.com",
+        "image": "icon1.png",
+        "title": "card1",
+        "zoom": "1",
+        "type": "web",
+        "section": "card2"
+    }
+]
